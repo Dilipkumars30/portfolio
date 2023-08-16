7fc8280ed706d407e430a32f57ca8cbcaa6df1f9
Dilipkumars30 <uee19110@rmd.ac.in>
2023-08-16 22:48:13 +0530

    few change
    naw

--- script.js
+++ script.js
@@ -65,31 +65,6 @@ panels.forEach(panel => {
 });
 
 
-//paralex/
-
-
-//const observer = new IntersectionObserver((entries)=>{
-   // entries.forEach((entry)=>{
-      //  if(entry.isIntersecting){
-      //      entry.target.classList.add("show-items");
-        //}else{
-          //  entry.target.classList.remove("show-items");
-
-       // }
-   // });
-
-//});
-
-//const scrollScale = document.querySelectorAll(".scroll-scale");
-//scrollScale.forEach((el)=>observer.observer(el));
-
-//const scrollBottom = document.querySelectorAll(".scroll-bottom");
-//scrollBottom.forEach((el)=>observer.observer(el));
-
-//const scrollTop = document.querySelectorAll(".scroll-top");
-//scrollTop.forEach((el)=>observer.observer(el));
-
-
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
